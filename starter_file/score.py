5bc3146c44aa55e8a375c9b1a77c118811beb6c5
alathiya <alathiya@dxc.com>
2020-09-29 17:35:27 -0400

    updated automl notebook to change model deployment to local webservice

--- starter_file/score.py
+++ starter_file/score.py
@@ -1,4 +1,4 @@
-%%writefile score.py
+##%%writefile score.py
 import json
 import numpy as np
 import os
@@ -10,12 +10,22 @@ def init():
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
     # For multiple models, it points to the folder containing all deployed models (./azureml-models)
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.joblib')
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'best-trained-model.pkl')
     model = joblib.load(model_path)
 
-def run(raw_data):
-    data = np.array(json.loads(raw_data)['data'])
-    # make prediction
-    y_hat = model.predict(data)
-    # you can return any data type as long as it is JSON-serializable
-    return y_hat.tolist()
+#def run(raw_data):
+#    data = np.array(json.loads(raw_data)['data'])
+#    # make prediction
+#    y_hat = model.predict(data)
+#    # you can return any data type as long as it is JSON-serializable
+#    return y_hat.tolist()
+
+def run(data):
+    try:
+        data = np.array(json.loads(data))
+        result = model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
